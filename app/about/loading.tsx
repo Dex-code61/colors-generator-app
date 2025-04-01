b5c1cae5b4e68da9228cbfea73be064bbf99d1e7
Dex-code61 <deodatkanabo@gmail.com>
2025-04-01 08:17:26 +0000

    ended 3

--- app/about/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  // Or a custom loading skeleton component
-  return (
-    <div className="w-full h-full flex items-center justify-center">
-      <h1>Loading...</h1>
-    </div>
-  );
-}
