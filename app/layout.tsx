297109170b2e9252169fbbcee5dd9e1c2248800f
Dex-code61 <deodatkanabo@gmail.com>
2025-04-01 00:52:02 +0000

    derniers changement 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/navBar";
 import Link from "next/link";
-import ErrorBoundary from "./ErrorBoundary";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,20 +29,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} dark relative max-h-screen flex-col antialiased flex items-center justify-center w-full h-screen`}
       >
-        <ErrorBoundary>
-          <NavBar />
-          <div className="w-full h-full">{children}</div>
-          <div className="w-full z-50 text-foreground text-sm font-light py-1 px-3 text-center">
-            Colors generator - 2025 by{" "}
-            <Link
-              className="font-medium"
-              target="_blank"
-              href="https://github.com/Dex-code61/colors-generator-app"
-            >
-              Dex-code61
-            </Link>
-          </div>
-        </ErrorBoundary>
+        <NavBar />
+        <div className="w-full h-full">{children}</div>
+        <div className="w-full z-50 text-foreground text-sm font-light py-1 px-3 text-center">
+          Colors generator - 2025 by{" "}
+          <Link
+            className="font-medium"
+            target="_blank"
+            href="https://github.com/Dex-code61/colors-generator-app"
+          >
+            Dex-code61
+          </Link>
+        </div>
       </body>
     </html>
   );
